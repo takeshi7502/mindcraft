91d55ae76fda1e37ea3383cac647001df6ceb7aa
takeshi7502 <takeshi.gi75@gmail.com>
2026-09-23 23:00:48 +0700

    Add OmniRoute model adapter

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const settings = {
     "minecraft_version": "auto", // or specific version like "1.21.6"
-    "host": "mc.jinmups.com", // or "localhost", "your.ip.address.here"
+    "host": "mine.210z.dev", // or "localhost", "your.ip.address.here"
     "port": 25565, // set to -1 to automatically scan for open ports
     "auth": "offline", // or "microsoft"
 
